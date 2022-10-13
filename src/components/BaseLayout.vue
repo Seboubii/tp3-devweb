041fef572076e00fb02c8207ba916d6b2cfffea6
Seboubii <sebastien.latronche@efrei.net>
2022-10-13 19:21:49 +0200

    ex 5

---
+++ src/components/BaseLayout.vue
@@ -0,0 +1,38 @@
+
+<template>
+
+    <div>
+        <base-header>ratio</base-header> 
+        <home-page>test</home-page>
+        <base-footer>ratiette</base-footer> 
+    </div>
+
+</template>
+
+<style scoped>
+div {
+  min-height: 98vh;
+  display:flex;
+  flex-direction: column;
+}
+
+home-page {
+  display: flex;
+  flex-grow: 1;
+  align-items: center;
+  justify-content: center;
+}
+
+base-header {
+  background-color: #42b983;
+  color : #660ca7;
+  justify-content: center;
+  display: flex;
+}
+base-footer {
+  background-color: #660ca7;
+  color : #42b983;
+  justify-content: center;
+  display: flex;
+}
+</style>
